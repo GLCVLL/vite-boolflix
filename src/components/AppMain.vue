0b84b49aa8c14b00f33d298472383e64622d2c3c
GLCVLL <g.ioele96@outlook.it>
2023-07-05 19:23:58 +0200

    added new components

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,9 +1,14 @@
 <script>
+import AppMoviesList from './AppMoviesList.vue';
 export default {
-
+    components: { AppMoviesList },
 };
 </script>
 
-<template></template>
+<template>
+    <main>
+        <AppMoviesList />
+    </main>
+</template>
 
 <style scoped lang="scss"></style>
--- src/data/index.js
+++ src/data/index.js
@@ -1,4 +1,4 @@
-const baseUri = 'https://api.themoviedb.org/3';
+
 
 const apiKey = '8c861616e007fa4b4b7339acd654cb20';
 
