55e72a332e6c83a2516eca097727498a1e3bff2f
GLCVLL <g.ioele96@outlook.it>
2023-07-05 18:00:20 +0200

    added axios and components

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,19 @@
 <script>
+import AppHeader from './components/Appheader.vue';
+import AppMain from './components/AppMain.vue';
 export default {
+  components: { AppHeader, AppMain },
+  methods: {
+
+  }
 
 }
 </script>
 
-<template></template>
+<template>
+  <AppHeader />
+  <AppMain />
+</template>
 
 <style lang="scss">
 @use './assets/scss/style.scss'
---
+++ src/components/SearchForm.vue
@@ -0,0 +1,34 @@
+<script>
+export default {
+    props: {
+        placeholder: String,
+    },
+    data() {
+        return {
+            searchTerm: '',
+        }
+    },
+    methods: {
+        submitForm() {
+            this.$emit('form-submit', this.searchTerm)
+        },
+        resetForm() {
+            this.searchTerm = '';
+            this.submitform();
+        }
+    },
+    emits: ['form-submit'],
+};
+</script>
+
+<template>
+    <div class="container">
+        <form class="d-flex" @submit.prevent="submitForm">
+            <input type="text" :placeholder="placeholder || 'Cerca...'" class="form-control mx-5" v-model.trim="searchTerm">
+            <button class="btn btn-success">Cerca</button>
+            <button class="btn btn-warning ms-3" @click="resetForm">Annulla</button>
+        </form>
+    </div>
+</template>
+
+<style scoped lang="scss"></style>
